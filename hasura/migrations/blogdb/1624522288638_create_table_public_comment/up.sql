e32d04104459ae056231cfce4fad0301165c9b41
Rishichandra Wawhal <rishi@hasura.io>
2021-06-24 13:42:18 +0530

    add a comment table

---
+++ hasura/migrations/blogdb/1624522288638_create_table_public_comment/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."comment" ("id" serial NOT NULL, "body" text NOT NULL, "author_id" integer NOT NULL, "article_id" integer NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("article_id") REFERENCES "public"."article"("id") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("author_id") REFERENCES "public"."author"("id") ON UPDATE restrict ON DELETE restrict);
